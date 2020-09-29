03f4a1fe6dd6e664f5fd2be21b33cd61aca3b006
Riley <rag16c@my.fsu.edu>
2020-09-28 20:00:57 -0400

    Added Filtering and Firebase files

--- components/CourseList.js
+++ components/CourseList.js
@@ -1,14 +1,28 @@
 import Course from './Course';
-import React from 'react';
+import React, {useState, useEffect } from 'react';
 import {ScrollView, View, StyleSheet } from 'react-native';
+import TermSelector from './TermSelector';
 
-const CourseList = ({courses}) => (
-    <ScrollView>
-      <View style={styles.courseList}>
-        { courses.map(course => <Course key={course.id} course={course} />) }
-      </View>
-    </ScrollView>
+  const termMap = { F: 'Fall', W: 'Winter', S: 'Spring'};
+  const terms = Object.values(termMap);
+  
+  const getCourseTerm = course => (
+  termMap[course.id.charAt(0)]
 );
+
+const CourseList = ({courses}) => {
+	
+  const [selectedTerm, setSelectedTerm] = useState('Fall');
+
+  const termCourses = courses.filter(course => selectedTerm === getCourseTerm(course));
+  
+  return (
+    <ScrollView>
+      <TermSelector selectedTerm={selectedTerm} setSelectedTerm={setSelectedTerm}/>
+      { termCourses.map(course => <Course key={course.id} course={course} />) }
+    </ScrollView>
+  );
+};
 
 const styles = StyleSheet.create({
   courseList: {
