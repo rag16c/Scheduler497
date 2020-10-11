2555a082b5d216c20134890b260675b54e966fa2
Riley <rag16c@my.fsu.edu>
2020-10-10 21:07:20 -0400

    Store data

--- screens/CourseEditScreen.js
+++ screens/CourseEditScreen.js
@@ -1,7 +1,9 @@
-import React from 'react';
-import Form from '../components/Form';
-import * as Yup from 'yup';
+import React, {useState} from 'react';
 import { SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native';
+import * as Yup from 'yup';
+import { Formik } from 'formik';
+import Form from '../components/Form';
+import { firebase } from '../firebase';
 
 const Field = ({label, value}) => {
   return (
@@ -12,32 +14,29 @@ const Field = ({label, value}) => {
   );
 };
 
-const validationSchema = Yup.object().shape({
-    id: Yup.string()
-      .required()
-      .matches(/(F|W|S)\d{3,}/, 'Must be a term and 3-digit number')
-      .label('ID'),
-    meets: Yup.string()
-      .required()
-      .matches(/(M|Tu|W|Th|F)+ +\d\d?:\d\d-\d\d?:\d\d/, 'Must be weekdays followed by start and end time')
-      .label('Meeting times'),
-    title: Yup.string()
-      .required()
-      .label('Title'),
-  });
-
-const CourseEditScreen = ({route}) => {
+const CourseEditScreen = ({ navigation, route }) => {
   const course = route.params.course;
+  const [submitError, setSubmitError] = useState('');
+
+  async function handleSubmit(values) {
+    const { id, meets, title } = values;
+    const course = { id, meets, title };
+    firebase.database().ref('courses').child(id).set(course).catch(error => {
+      setSubmitError(error.message);
+    });
+  }
+
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView>
-      <Form
+        <Form
           initialValues={{
             id: course.id,
             meets: course.meets,
             title: course.title,
           }}
           validationSchema={validationSchema}
+          onSubmit={values => handleSubmit(values)}
         >
           <Form.Field
             name="id"
@@ -57,18 +56,35 @@ const CourseEditScreen = ({route}) => {
             leftIcon="format-title"
             placeholder="Introduction to programming"
           />
+          <Form.Button title={'Update'} />
+          {<Form.ErrorMessage error={submitError} visible={true} />}
         </Form>
       </ScrollView>
     </SafeAreaView>
   );
 };
 
+const validationSchema = Yup.object().shape({
+  id: Yup.string()
+    .required()
+    .matches(/(F|W|S)\d{3,}/, 'Must be a term and 3-digit number')
+    .label('ID'),
+  meets: Yup.string()
+    .required()
+    .matches(/(M|Tu|W|Th|F)+ +\d\d?:\d\d-\d\d?:\d\d/, 'Must be weekdays followed by start and end time')
+    .label('Meeting times'),
+  title: Yup.string()
+    .required()
+    .label('Title'),
+});
+
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    // alignItems: 'center',
+    alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: '#ccccb3',
+    backgroundColor: '#ccccb3'
   },
   field: {
     height: 40,
